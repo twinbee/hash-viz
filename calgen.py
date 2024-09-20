9125d9e2b978380c1266c44230c25caf01b68c3d
Matthew Bennett <matt.bennett@missecurity.com>
2024-09-19 19:27:53 -0500

    calgen fix issue with blank dates to align at begin of months

--- calgen.py
+++ calgen.py
@@ -83,7 +83,6 @@ def generate_tsv_events(month, year):
     events.sort(key=lambda x: x['date'])
     return events
 
-
 # Function to generate TSV format content
 def generate_tsv_file(month, year, events):
     header = "DAY\tKENNEL\tICON\tTITLE\tRUN\tHARES\tTIME\tSTART\tMAP\tHASHCASH\tTURDS\tTWEET\tTWILIGHT\tDATE\tDESC\tUPDATE"
@@ -93,7 +92,6 @@ def generate_tsv_file(month, year, events):
         rows.append(row)
     return "\n".join(rows)
 
-# Function to generate a PHP calendar file template for a specific month and year
 # Function to generate a PHP calendar file template for a specific month and year
 def generate_php_file(month, year, previous_month_link, next_month_link):
     # Define the month name and month image
@@ -161,105 +159,60 @@ if (d.getYear() % 100 == {str(year)[-2:]}) document.write('<style type="text/css
 """
     return php_content
 
-
-    # Define the month name and month image
-    month_name = calendar.month_name[month]
-    month_image = f"month-{str(month).zfill(2)}.png"
-    
-    # HTML and PHP content for the month
-    php_content = f"""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="pragma" content="no-cache" />
-<meta http-equiv="CACHE-CONTROL" content="NO-CACHE" />
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-<meta name="generator" content="Martha's Calendar Generator" />
-<title>{month_name}, {year} Hash Events</title>
-<link href="calendar.css" rel="stylesheet" type="text/css" media="all" />
-<script language="JavaScript">
-var d = new Date();
-var id = "j" + d.getMonth() + d.getDate();
-if (d.getYear() % 100 == {str(year)[-2:]}) document.write('<style type="text/css" media="screen"><!-- table.inner#' + id + ' {{ background-image: url(throb.gif); }}--> </style>');
-</script>
-<?php
-    $year={year};
-    $month={month};
-    include 'php.php';
-?>
-</head>
-<body>
-<map name="Map" id="Map">
-    <area shape="rect" coords="0,0,150,91" href="{previous_month_link}" alt="Previous Month" />
-    <area shape="rect" coords="957,0,807,91" href="{next_month_link}" alt="Next Month" />
-</map>
-<div class=container>
-    <table class="overall" border="0" cellspacing="0" cellpadding="0">
-        <tr>
-            <td>
-                <table class="banner" border="0" cellspacing="0" cellpadding="0">
-                    <tr>
-                        <td><img src="{month_image}" alt="{month_name}, {year}" border="0" usemap="#Map" /></td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <table class="main" border="0" cellspacing="0" cellpadding="0">
-                    <tr>
-                        <th>Sunday</th>
-                        <th>Monday</th>
-                        <th>Tuesday</th>
-                        <th>Wednesday</th>
-                        <th>Thursday</th>
-                        <th>Friday</th>
-                        <th>Saturday</th>
-                    </tr>
-                    <!-- Events will go here -->
-                    {generate_event_rows(events)}
-                </table>
-            </td>
-        </tr>
-    </table>
-</div>
-</body>
-</html>
-"""
-    return php_content
-
-# Function to generate event rows for the PHP file
 def generate_event_rows(month, year):
-    month_calendar = calendar.monthcalendar(year, month)
+    # Get the day of the week of the 1st day of the month and the number of days in the month
+    first_day_of_week, days_in_month = calendar.monthrange(year, month)  # 0=Monday, 6=Sunday
     rows = []
-    
-    # Iterate through each week in the month
-    for week in month_calendar:
-        row = '<tr>\n'
-        for day in week:
-            if day == 0:  # This represents an empty day (non-day)
-                row += '\t<td class="empty"></td>\n'
-            else:  # Regular day with a date
-                row += f'''\t<td class="day">
-                            <table class="inner" id="j{month}{str(day).zfill(2)}">
+
+    # Start generating the calendar row by row
+    current_row = '<tr>\n'
+
+    # Add empty cells before the first day of the month (Sunday is 6, so we need to map 0=Monday to 1=Sunday)
+    for _ in range(first_day_of_week + 1):
+        current_row += '\t<td class="empty"></td>\n'
+
+    # Start from the 1st of the month
+    day_counter = 1
+
+    # Fill in the rest of the days for the current row
+    while day_counter <= days_in_month:
+        # Add a day cell
+        current_row += f'''\t<td class="day">
+                            <table class="inner" id="j{month}{str(day_counter).zfill(2)}">
                                 <tr>
-                                    <td class="dom">{day}</td>
+                                    <td class="dom">{day_counter}</td>
                                 </tr>
                                 <tr>
                                     <td class="event">
-                                        <?php fillIn({month}, {day}, {year}); ?>
+                                        <?php fillIn({month}, {day_counter}, {year}); ?>
                                     </td>
                                 </tr>
                             </table>
                         </td>\n'''
-        row += '</tr>\n'
-        rows.append(row)
-    
+        # Move to the next day
+        day_counter += 1
+
+        # If the current row is full (7 columns), close it and start a new row
+        if (first_day_of_week + day_counter - 1) % 7 == 0 or day_counter > days_in_month:
+            current_row += '</tr>\n'
+            rows.append(current_row)
+            current_row = '<tr>\n'
+
+    # If there are remaining cells in the last row, fill them with empty cells
+    if current_row != '<tr>\n':  # Check if the last row has content
+        while (first_day_of_week + day_counter - 1) % 7 != 0:
+            current_row += '\t<td class="empty"></td>\n'
+            day_counter += 1
+        current_row += '</tr>\n'
+        rows.append(current_row)
+
     return "".join(rows)
 
 
 # Function to generate both TSV and PHP files for a month
 def generate_files_for_month(month, year, previous_month_link, next_month_link, events):
     tsv_content = generate_tsv_file(month, year, events)
+    # The events are no longer needed in the php_content generation since the PHP handles it dynamically
     php_content = generate_php_file(month, year, previous_month_link, next_month_link)
     
     # File paths
